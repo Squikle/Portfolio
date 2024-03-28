f952c513510b7d7d147b31f2dce7096247964d6b
Squikle <mmishaa1702@gmail.com>
2024-03-29 00:25:43 +0100

    removed extra code

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import { useEffect } from "react";
 import ParallaxPage from "./Pages/ParallaxPage.tsx";
 import LogoPage from "./Pages/LogoPage.tsx";
-import { useCustomScroll } from "./hooks/useCustomScroll.ts";
 
 export default function App() {
-  //useCustomScroll();
   useEffect(() => {
     window.onbeforeunload = function () {
       window.scrollTo(0, 0);
