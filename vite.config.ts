eb0822d11d340a05efdf328918c7c66318349ef8
Squikle <mmishaa1702@gmail.com>
2024-04-05 20:00:21 +0200

    png removed

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import viteCompression from "vite-plugin-compression";
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "",
-  plugins: [react(), viteCompression()],
+  plugins: [react(), viteCompression(), visualizer()],
   optimizeDeps: {
     exclude: ["azysizes_plugins_blur-up_ls__blur-up"],
+  },
+  build: {
+    sourcemap: false,
   },
 });
