3a3ad86da86b9997de53ecd7f1a01c2d23a4ba6e
Squikle <mmishaa1702@gmail.com>
2024-04-13 23:37:06 +0200

    added plugins for compressing

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,36 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import viteCompression from "vite-plugin-compression";
 import { visualizer } from "rollup-plugin-visualizer";
+import compression from "vite-plugin-compression2";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
+
+const imageOptimizer = ViteImageOptimizer({
+  png: {
+    quality: 50,
+    compressionLevel: 9,
+    effort: 9,
+  },
+  webp: {
+    quality: 50,
+    lossless: false,
+    effort: 6,
+  },
+  exclude: /.*sky.*/,
+});
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "",
-  plugins: [react(), viteCompression(), visualizer()],
+  plugins: [
+    react(),
+    visualizer(),
+    imageOptimizer,
+    compression({
+      algorithm: "brotliCompress",
+      exclude: [/\.(br)$/, /\.(gz)$/],
+      compressionOptions: {},
+    }),
+  ],
   optimizeDeps: {
     exclude: ["azysizes_plugins_blur-up_ls__blur-up"],
   },
