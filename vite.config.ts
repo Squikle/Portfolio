c737bd652bb51c173e6a63d762bfb3419380aa3f
Squikle <mmishaa1702@gmail.com>
2024-04-16 23:48:05 +0200

    @ root path set up

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import { visualizer } from "rollup-plugin-visualizer";
 import compression from "vite-plugin-compression2";
 import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
+import * as path from "node:path";
 
 const imageOptimizer = ViteImageOptimizer({
   png: {
@@ -16,7 +17,7 @@ const imageOptimizer = ViteImageOptimizer({
     effort: 6,
   },
 });
-
+console.log(path.resolve(__dirname, 'src'));
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "",
@@ -30,6 +31,7 @@ export default defineConfig({
       compressionOptions: {},
     }),
   ],
+
   optimizeDeps: {
     exclude: ["azysizes_plugins_blur-up_ls__blur-up"],
   },
@@ -37,5 +39,12 @@ export default defineConfig({
     sourcemap: false,
     minify: "terser",
     cssMinify: "esbuild",
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@components': path.resolve(__dirname, './src/components'),
+    },
   },
 });
